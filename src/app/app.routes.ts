0baaf2d25b3358c732597db3f8d3002c7d9058f4
evp0005Malakai <evp0005@alu.medac.es>
2025-10-30 12:16:31 +0100

    AUTH-004: Implementa layout base de Home Dashboard con Angular Material y paleta corporativa.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // ... (aquí las rutas)
+
+  // RUTA PARA LA INTERFAZ HOME (AUTH-004)
+  {
+    path: 'home',
+    loadComponent: () =>
+    import('./features/home/pages/home/home.component').then(
+      (m) => m.HomeComponent // Uso estándar recomendado
+    ),
+  },
+
+  // Para que la raíz redirija a home (se puede borrar luego si eso)
+  {
+     path: '',
+     redirectTo: 'home',
+     pathMatch: 'full',
+  },
+
+  // ... (Continuar con otras rutas)
+];
---
+++ src/app/features/home/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html', // Corregido
+  styleUrl: './home.component.scss',   // Corregido
+})
+export class HomeComponent {
+  // ...
+}
