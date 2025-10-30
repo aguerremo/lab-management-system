ff7994167d13346edea262aa90dfcb4d280d58b0
MiguelTorralvo0020 <mtg0020@alu.medac.es>
2025-10-30 09:45:59 +0100

    ActualizacionRutasyApp.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './features/auth/login/login/login';
 export const routes: Routes = [
 {
   // Ruta para el componente de login
-  path: 'login',
+  path: '',
   component:LoginComponent,
 }
 
